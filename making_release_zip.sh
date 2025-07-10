d5918ef0265481ff7bd36edbf2aa557751b9a26f
RanaUniverse <RanaUniverse321@gmail.com>
2025-07-10 13:12:04 +0530

    making zip in the release got updated goodly

--- making_release_zip.sh
+++ making_release_zip.sh
@@ -4,6 +4,6 @@ APP_VERSION="25.2.4"
 
 repo_name=$(basename "$(pwd)")
 
-zip -r "${repo_name}_${APP_VERSION}_$VERSION.zip" . -x "*.git*"
+zip -r "${repo_name}_$VERSION_$APP_VERSION.zip" . -x "*.git*"
 
 echo "This zip file is made for the release of this repo, i.e., $repo_name."
