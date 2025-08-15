e0f3ac133cab824bc5260869c32eaec30ad7a774
RanaUniverse <RanaUniverse321@gmail.com>
2025-08-15 18:48:34 +0530

    making zip name will be good

--- making_release_zip.sh
+++ making_release_zip.sh
@@ -1,9 +1,12 @@
 
+
+# Before making the zip i need to make sure change the version name and run this script.
+
 VERSION="v1.1.0"
 APP_VERSION="25.2.5"
 
 repo_name=$(basename "$(pwd)")
 
-zip -r "${repo_name}_${VERSION}_${APP_VERSION}.zip" . -x "*.git*"
+zip -r "${repo_name}_${VERSION}_LibreOffice_${APP_VERSION}.zip" . -x "*.git*"
 
 echo "This zip file is made for the release of this repo, i.e., $repo_name."
